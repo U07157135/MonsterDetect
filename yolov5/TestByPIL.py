cfc63c2dabf6ab72e911fdf090034e001877e494
U07157135 <jason65050449@gmail.com>
2022-01-25 18:21:34 +0800

    2022/1/25

--- yolov5/TestByPIL.py
+++ yolov5/TestByPIL.py
@@ -6,7 +6,7 @@ from PIL import Image
 cwd = os.getcwd()
 model = torch.hub.load('ultralytics/yolov5', 'custom',path = cwd+"\\yolov5\\model\\last.pt",device="0")
 
-img = Image.open('img\\test.png')
+img = Image.open('yolov5\\img\\test.png')
 img = model(img)
 img = img.render()[0]
 img = Image.fromarray(img)
